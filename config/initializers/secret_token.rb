80c2a4319fd66ad0fc644233285b5efccb6dd467
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-06-10 18:43:54 +0200

    initial commit with static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piupiu::Application.config.secret_token = 'cc15be1718f1b555a9e5e53c3d0c6ff5e5465738a6b33143c9bb49974a8fe30db0719186ef65928114d322ec62719fe42c901195a40439fc5f099502362e1dc3'
