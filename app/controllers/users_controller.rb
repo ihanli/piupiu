7438b494620a1815695cd6adf719af023c4ed549
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-06-12 21:07:39 +0200

    user_root is used to redirect after sign in, instead of application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   
+  def index
+    @user = User.find_by_id(current_user.id)
+  end
+  
   def show
     @user = User.find_by_id(params[:id])
   end
