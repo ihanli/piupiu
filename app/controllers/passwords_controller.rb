17721e3e529838788a58c428c5c528a421de8a29
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-07-03 18:45:36 +0200

    added missing pages, configured action mailer and redirects

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,20 @@
+class PasswordsController < Devise::PasswordsController
+  # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(params[resource_name])
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :send_instructions) if is_navigational_format?
+      respond_with resource, :location => after_sending_reset_password_instructions_path_for(resource_name)
+    else
+      respond_with_navigational(resource){ render_with_scope :new }
+    end
+  end
+
+  protected
+
+  # The path used after sending reset password instructions
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    page_path("check_mail")
+  end
+end
