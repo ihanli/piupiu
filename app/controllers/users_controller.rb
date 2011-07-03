751539c048ab262e91e3f10327409c5fbc908b93
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-07-03 18:45:36 +0200

    added missing pages, configured action mailer and redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,5 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @user = User.find_by_id(current_user.id)
-  end
-  
-  def show
-    @user = User.find_by_id(params[:id])
   end
 end
