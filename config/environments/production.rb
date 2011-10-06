b65bff64f901f8107dd3810f021c82648a5f376e
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-10-07 01:47:33 +0200

    playin around

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Piupiu::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => "piupiu.at" }
+  config.action_mailer.default_url_options = { :host => "beta.piupiu.at" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   
