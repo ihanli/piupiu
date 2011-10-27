ea9c165c4cbe74f6c22623d70dcfcc94fc224b8b
Ismail Hanli <iha@betterplace.org>
2011-10-27 17:44:25 +0200

    redirect to error page with correct status code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def render_optional_error_file(status_code)
+    if status_code == 500
+      render :template => "pages/500", :status => 500, :layout => "error"
+    elsif status_code == 404
+      render :template => "pages/404", :status => 404, :layout => "error"
+    end
+  end
 end
