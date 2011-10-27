aac70b5ce2e2b003d3578f0d0e89823e9da47f06
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-10-27 21:59:06 +0200

    profile page of other users can be displayed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,16 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @user = current_user
     @posts = Post.roots.find_all_by_user_id(current_user.id)
     @comments = Post.comments.find_all_by_user_id(current_user.id)
+  end
+  
+  def show
+    @user = User.find_by_id(params[:id])
+    @posts = Post.roots.find_all_by_user_id(@user.id)
+    @comments = Post.comments.find_all_by_user_id(@user.id)
+    
+    render :action => :index
   end
 end
