5601ea33664abe8217d91531fb3dbb68ae755547
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2011-11-16 21:06:50 +0100

    using old filename interpolation

--- app/models/post.rb
+++ app/models/post.rb
@@ -68,9 +68,7 @@ class Post < ActiveRecord::Base
   end
   
   def normalized_image_file_name
-    nomalized_file_name = self.image_file_name[0, self.image_file_name.rindex(".")]
-    file_extension = self.image_file_name[self.image_file_name.rindex("."), self.image_file_name.length]
-    "#{self.id}-#{nomalized_file_name}#{file_extension}" 
+    "#{self.id}-#{self.image_file_name.gsub([^a-zA-Z0-9_\.], "_")}" 
   end
   
   private
