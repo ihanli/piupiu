e4d944f6b2918cb9cb87bfb8baa918a3121b20d7
Ismail Hanli <ismail.hanli@hotmail.com>
2012-02-20 14:27:04 +0100

    bug fix: error thrown if user is logged out

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,6 +1,6 @@
 SimpleNavigation::Configuration.run do |navigation|  
   navigation.items do |primary|
-    primary.item :profile, image_tag(Proc.new{ current_user.avatar.url(:icon) }.call), user_root_path, :class => "rahmen_profilbild", :if => Proc.new{ user_signed_in? }
+    primary.item :profile, Proc.new{ image_tag(current_user.avatar.url(:icon)) if current_user }.call, user_root_path, :class => "rahmen_profilbild", :if => Proc.new{ user_signed_in? }
     primary.item :posts, "", posts_path, :class => "overview_link", :if => Proc.new{ user_signed_in? }
     primary.item :new_post, "", new_post_path, :class => "new_post", :if => Proc.new{ user_signed_in? }
     primary.item :logout, "", destroy_user_session_path, :class => "logout", :if => Proc.new{ user_signed_in? }
